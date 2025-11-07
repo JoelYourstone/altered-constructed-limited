46bdd79941dd035396823581aa557ec4cff48e94
JoelYourstone <joel.yourstone@gmail.com>
2025-11-07 21:26:17 +0100

    protect the import page

--- src/app/my-vault/import-csv/page.tsx
+++ src/app/my-vault/import-csv/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@/auth";
 import ImportCsvModule from "./ImportCsvModule";
+import { redirect } from "next/navigation";
 
-export default function ImportCsvPage() {
+export default async function ImportCsvPage() {
+  const session = await auth();
+  if (!session?.user) {
+    return redirect("/api/auth/signin?callbackUrl=/my-vault/import-csv");
+  }
+
   return <ImportCsvModule />;
 }
-
