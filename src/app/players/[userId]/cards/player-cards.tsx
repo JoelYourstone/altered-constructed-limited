ff4df576052e040c24b5d9112b853fb1c3c2f1ab
JoelYourstone <joel.yourstone@gmail.com>
2025-11-13 00:19:55 +0100

    DRY. Or just RY, i'm just a commit message, not a cop

--- src/app/players/[userId]/cards/player-cards.tsx
+++ src/app/players/[userId]/cards/player-cards.tsx
@@ -3,18 +3,20 @@ import { useVaultState } from "@/hooks/useVault";
 import Link from "next/link";
 import type { CardViewData } from "@/app/api/vault/state/route";
 import ThinCard from "@/components/ThinCard";
+import { useState } from "react";
 
 export default function PlayerCards({ userId }: { userId: string }) {
   console.log("userId", userId);
   const { data: vault, isLoading, error } = useVaultState(userId);
 
+  const [showImages, setShowImages] = useState(false);
   const allBoosters = [
     ...(vault?.activeBoosters ?? []),
     ...(vault?.completedBoosters ?? []),
   ];
 
   // Collect all cards from all boosters
-  const allCards: CardViewData[] = allBoosters
+  let allCards: CardViewData[] = allBoosters
     .flatMap((booster) => booster.cards)
     .sort((a, b) => {
       const rarityOrder = ["UNIQUE", "RARE", "COMMON"];
@@ -31,6 +33,61 @@ export default function PlayerCards({ userId }: { userId: string }) {
       }
       return 0;
     });
+
+  // filters
+  // allCards = allCards.filter(
+  //   (card) =>
+  //     card.card_data.cardSet.name.toLowerCase() === "whispers from the maze" &&
+  //     card.card_data.cardType.reference !== "HERO" &&
+  //     card.card_data.mainFaction.name.toLowerCase() === "yzmir"
+  // );
+
+  if (
+    globalThis.document &&
+    globalThis.document.location.search.includes("sealed")
+  ) {
+    allCards = allCards.filter(
+      (card) =>
+        card.card_data.cardSet.name.toLowerCase() === "skybound odyssey" &&
+        // card.card_data.cardType.reference !== "HERO" &&
+        // card.card_data.mainFaction.name.toLowerCase() === "yzmir"
+        true &&
+        new Date(card.scanned_at) >= new Date("2025-11-12T00:00:00Z")
+    );
+  }
+
+  const factionsNameLowerCase = [
+    "yzmir",
+    "bravos",
+    "axiom",
+    "lyra",
+    "muna",
+    "ordis",
+  ];
+
+  const factionStatistics = factionsNameLowerCase.map((faction) => {
+    return {
+      name: faction,
+      count: allCards.filter(
+        (card) => card.card_data.mainFaction.name.toLowerCase() === faction
+      ).length,
+      distinctHeroes: allCards.filter(
+        (card) =>
+          card.card_data.mainFaction.name.toLowerCase() === faction &&
+          card.card_data.cardType.reference === "HERO"
+      ).length,
+      rares: allCards.filter(
+        (card) =>
+          card.card_data.mainFaction.name.toLowerCase() === faction &&
+          card.card_data.rarity.reference === "RARE"
+      ).length,
+      uniques: allCards.filter(
+        (card) =>
+          card.card_data.mainFaction.name.toLowerCase() === faction &&
+          card.card_data.rarity.reference === "UNIQUE"
+      ).length,
+    };
+  });
 
   return (
     <div className="min-h-screen bg-background">
@@ -66,6 +123,37 @@ export default function PlayerCards({ userId }: { userId: string }) {
           </div>
         )}
 
+        {/* Faction statistics */}
+        <div className="mb-8">
+          <h2 className="text-lg font-semibold mb-4">Faction Statistics</h2>
+          <div className="grid grid-cols-4 gap-4">
+            {factionStatistics.map((faction) => (
+              <div key={faction.name}>
+                <p className="text-foreground/70">{faction.name}</p>
+                <p className="text-foreground/70">Total: {faction.count}</p>
+                <p className="text-foreground/70">
+                  Heroes: {faction.distinctHeroes}
+                </p>
+                <p className="text-foreground/70">Rares: {faction.rares}</p>
+                <p className="text-foreground/70">Uniques: {faction.uniques}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mb-8">
+          <button
+            type="button"
+            onClick={() => {
+              setShowImages(!showImages);
+            }}
+            className="rounded-full border border-solid border-foreground transition-colors inline-flex items-center justify-center bg-background text-foreground hover:bg-foreground hover:text-background font-medium text-sm px-4 py-2"
+            // This is a mock button for now, does not have functionality
+          >
+            Show images
+          </button>
+        </div>
+
         {/* Cards Display */}
         {!isLoading && !error && (
           <div className="bg-background border border-black/[.08] dark:border-white/[.145] rounded-lg">
@@ -77,9 +165,17 @@ export default function PlayerCards({ userId }: { userId: string }) {
               </div>
             ) : (
               <div className="grid grid-cols-2 gap-1 gap-y-2">
-                {allCards.map((card) => (
-                  <ThinCard key={card.unique_token} card={card.card_data} />
-                ))}
+                {!showImages
+                  ? allCards.map((card) => (
+                      <ThinCard key={card.unique_token} card={card.card_data} />
+                    ))
+                  : allCards.map((card) => (
+                      <img
+                        key={card.unique_token}
+                        src={card.card_data.imagePath}
+                        alt={card.card_data.name}
+                      />
+                    ))}
               </div>
             )}
           </div>
